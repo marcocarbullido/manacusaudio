a612f97b59466cb2417bea282f350f56b5b1907d
marcocarbullido <124408893+marcocarbullido@users.noreply.github.com>
2024-08-24 18:30:15 -0500

    Update spectrogram.py

--- audio_processing/spectrogram.py
+++ audio_processing/spectrogram.py
@@ -7,5 +7,5 @@ def spect(wav_filepath):
     D = librosa.stft(y, n_fft=y_res*2-1, hop_length=32)
     magnitude = np.abs(D)
     log_magnitude = librosa.amplitude_to_db(magnitude)
-    log_magnitude_normalized = (log_magnitude - np.mean(log_magnitude)) / np.std(log_magnitude)
-    return log_magnitude_normalized
+    z_log_magnitude = (log_magnitude - np.mean(log_magnitude)) / np.std(log_magnitude)
+    return z_log_magnitude
